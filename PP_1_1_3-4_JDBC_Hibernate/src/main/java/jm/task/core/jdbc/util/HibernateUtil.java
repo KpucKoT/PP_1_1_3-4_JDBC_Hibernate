7009496ee4cfe02ead6f93f7e28f13d5215d9c6d
KpucKoT <167256393+KpucKoT@users.noreply.github.com>
2024-07-11 16:42:41 +0300

    HibernateUtil.java

--- PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/util/HibernateUtil.java
+++ PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/util/HibernateUtil.java
@@ -20,8 +20,8 @@ public class HibernateUtil {
                 Properties settings = new Properties();
                 settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
                 settings.put(Environment.URL, "jdbc:mysql://localhost:3306/hibernate_db?useSSL=false");
-                settings.put(Environment.USER, "Kris");
-                settings.put(Environment.PASS, "Penguin020399");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "root");
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
 
                 settings.put(Environment.SHOW_SQL, "true");
